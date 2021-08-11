dde88c9b944858dd1715b98de1c6313a36c02116
Rokeya-Begum-Keya <87499132+Rokeya-Begum-Keya@users.noreply.github.com>
2021-08-10 21:42:11 -0600

    Updated TangentFunction.java
    Updated TangentFunction.java

--- SOEN-6011-Super-Calculator_Application/src/com/calculator/main/TangentFunction.java
+++ SOEN-6011-Super-Calculator_Application/src/com/calculator/main/TangentFunction.java
@@ -6,89 +6,108 @@ public class TangentFunction {
 		  return getTan(parseDouble);
 	}
 
-		//  variable start here
-		final static double PI = 3.14159265358979;
-		final static double EPS = 1E-2;
-		final static double EPSValueForMinimumCheck=1E-5;
+		
+//  variable start here
+	final static double PI = 3.14159265358979;
+	final static double EPS = 1E-2;
+	final static double EPSValueForMinimumCheck=1E-10;
+	// variable end here
+
+	// helper function or method start here
+	public static double getRad (double tempX){
+		return ((tempX*PI)/180.0);
+	}
+	public static double checkIfInsignificant (double tempX){
+		return tempX<0? tempX*(-1): tempX;
+	}
+	// helper function or method end ere
+
+	
+
+	// cos function or method start here
+	public static double getCos (double x){
+		x=getRad(x);
+		// variable start here
+		double retVal = 1;
+		double tmpResult = 1;
+		int i=1;
 		// variable end here
-
-		// helper function or method start here
-		public static double getRad (double tempX){
-			return ((tempX*PI)/180.0);
-		}
-		public static double checkIfInsignificant (double tempX){
-			return tempX<0? tempX*(-1): tempX;
-		}
-		// helper function or method end ere
-
 		
-
-		// cos function or method start here
-		public static double getCos (double x){
-
-			// variable start here
-			double retVal = 1;
-			double tmpResult = 1;
-			int i=1;
-			// variable end here
+		for (; ;i+=2){
 			
-			for (; ;i+=2){
-				
-				double value = (-1) * x *x / (i*(i+1));
-				
-				tmpResult = tmpResult * value;
-				
-				if (checkIfInsignificant(value)<=EPS) {
-					break;
-				}
-				
-				retVal =retVal+ tmpResult;
-			}
-			return retVal;
-		}
-		// cos function or method end here
-
-		
-		// sin function or method start here
-		public static double getSin(double x){
-
-			// variable start here
-			double tmpResult = x;
-			double retVal = x;
-			int i=0;
-			// variable end here
+			double value = (-1) * x *x / (i*(i+1));
 			
-			for(; ; ++i){
-				
-				double value = ((-1)*x*x / ((2*i+2)*(2*i+3)));
-				
-				tmpResult = tmpResult * value;
-				
-				if(checkIfInsignificant(value) <= EPS){
-					break;
-				}
-				
-				retVal=retVal+tmpResult;
+			tmpResult = tmpResult * value;
+			
+			if (checkIfInsignificant(value)<=EPS) {
+				break;
 			}
-			return retVal;
+			
+			retVal =retVal+ tmpResult;
 		}
-		// sin function or method end here
-		
+		return retVal;
+	}
+	// cos function or method end here
 
-		// tan function or method start here
-		public static String getTan (double x){
-			x=getRad(x);
-			double valueOfSinX=getSin(x);
-			double valueOfCosX=getCos(x);
+	
+	// sin function or method start here
+	public static double getSin(double x){
+		x=getRad(x);
+		// variable start here
+		double tmpResult = x;
+		double retVal = x;
+		int i=0;
+		// variable end here
+		
+		for(; ; ++i){
+			
+			double value = ((-1)*x*x / ((2*i+2)*(2*i+3)));
+			
+			tmpResult = tmpResult * value;
+			
+			if(checkIfInsignificant(value) <= EPS){
+				break;
+			}
+			
+			retVal=retVal+tmpResult;
+		}
+		return retVal;
+	}
+	// sin function or method end here
+	
+
+	// tan function or method start here
+	public static String getTan (double x){
+		double valueOfSinX=getSin(x);
+		double valueOfCosX=getCos(x);
+
+		//System.out.println(valueOfSinX);
+		//System.out.println(valueOfCosX);
+
+		
+		if(valueOfSinX>=0){
 			if(valueOfSinX<EPSValueForMinimumCheck){
 				return String.valueOf(0);
 			}
+		}
+		else{
+			if((-1*valueOfSinX)<EPSValueForMinimumCheck){
+				return String.valueOf(0);
+			}
+		}
+		
+		if(valueOfCosX>=0){
 			if(valueOfCosX<EPSValueForMinimumCheck){
 				return "undefined";
 			}
-			//System.out.println(valueOfSinX);
-			//System.out.println(valueOfCosX);
-			return String.valueOf(valueOfSinX/valueOfCosX);
+		}else{
+			if((-1*valueOfCosX)<EPSValueForMinimumCheck){
+				return "undefined";
+			}
 		}
+			
+		return String.valueOf(String.format("%.8f", (valueOfSinX/valueOfCosX)));
+	}
+	// tan function or method end here
 
 }
